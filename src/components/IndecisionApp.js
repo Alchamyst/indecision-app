df4bcaa27295ff15b79e469121b6191fe8ac30ad
Alchamyst <ashley@ashley-yetman.co.uk>
2020-07-06 14:29:32 +0100

    add OptionModal. Refactor stateless components to implcity return jsx.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -4,10 +4,12 @@ import Header from './Header';
 import Action from './Action';
 import AddOption from './AddOption';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
     };
     componentDidMount() {
         try {
@@ -43,7 +45,10 @@ export default class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option);
+        this.setState(() => ({ selectedOption: option }));
+    }
+    handleClearSelectedOption = () => {
+        this.setState(() => ({ selectedOption: undefined }));
     }
     handleAddOption = (option) => {
         if (!option) {
@@ -70,6 +75,10 @@ export default class IndecisionApp extends React.Component {
                 />
                 <AddOption 
                     handleAddOption = {this.handleAddOption}
+                />
+                <OptionModal 
+                    selectedOption = {this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
                 />
             </div>
         );
